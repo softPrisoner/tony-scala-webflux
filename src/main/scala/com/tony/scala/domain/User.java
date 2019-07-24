93676e96b28879a65d005b7e9979f8c538539aed
tony <wabslygzj@163.com>
2019-07-24 13:05:20 +0800

    init DTO class

---
+++ src/main/scala/com/tony/scala/domain/User.java
@@ -0,0 +1,64 @@
+package com.tony.scala.domain;
+
+import org.springframework.data.annotation.Id;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    @Id
+    private long id;
+    private String petName;
+    private int age;
+    private int sex;
+    private String username;
+    private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPetName() {
+        return petName;
+    }
+
+    public void setPetName(String petName) {
+        this.petName = petName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
